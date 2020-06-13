c91b63744b6c074772d6b32f124d7279d3cf0502
nanoshimarobot <1176641@st.anan-nct.ac.jp>
2020-06-14 01:19:55 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style_sheet.css" />
+	<title>はろーみなさま</title>
+</head>
+
+<body>
+	<h1 class="s1">はろーわーるどですわ～～～～～～～！！！！</h1>
+	<h1 class="s2">はろーわーるどですわ～～～～～～～！！！！</h1>
+	<h1 class="s3">はろーわーるどですわ～～～～～～～！！！！</h1>
+	<h1 class="s4">はろーわーるどですわ～～～～～～～！！！！</h1>
+</body>
+
+</html>
+<? php
+
+?>
