7330401b735edf6642124e8296a2f468fe5a51c7
nanoshimarobot <1176641@st.anan-nct.ac.jp>
2020-06-21 04:17:23 +0900

    addrの間違いを修正。socket通信の接続処理の実装始めた

--- index.php
+++ index.php
@@ -16,23 +16,30 @@
 
 </html>
 <?php
+error_reporting(E_ALL);
+set_time_limit(0);
+ob_implicit_flush();
+
 	class packet_format{
 		public $type;
 		public $message;
-		public $recv_data;
 	}
 
 	$recv_data = new packet_format;
+	$addr = '192.168.3.9';
 	$port = 49158;
-	$sock = socket_create(AF_INET, SOCK_DGRAM,SOL_UDP);
-	socket_bind($sock,'192.168.3.6',$port);
+	if(($sock = socket_create(AF_INET, SOCK_DGRAM,SOL_UDP)) === false){
+		echo "socket_create() failed: reason" . socket_strerror(socket_last_error()) . "\n";
+	}
+	if((socket_bind($sock,$addr,$port)) === false){
+		echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+	}
 	$from = '';
 	while(1){
 		socket_recvfrom($sock, $buf, 4096, 0, $from, $port);
-		$packet_data = unpack("Stype/Smessage/ITimestamp",$buf);
-		$recv_data->type = $array["type"];
-		$recv_data->message = $array["message"];
-		$recv_data->recv_data = data
+		$packet_data = unpack("Stype/Smessage",$buf);
+		$recv_data->type = $packet_data["type"];
+		$recv_data->message = $packet_data["message"];
 	}
 	
 ?>
