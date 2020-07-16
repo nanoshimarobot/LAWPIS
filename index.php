60bb40078e63b17469271a2aeb1ce129d79235cc
nanoshimarobot <1176641@st.anan-nct.ac.jp>
2020-07-17 00:52:11 +0900

    UI design is (maybe) complete

--- index.php
+++ index.php
@@ -3,21 +3,37 @@
 
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="/style_sheet/style_sheet.css" />
+	<link rel="stylesheet" type="text/css" href="style_sheet.css" />
 	<title>index</title>
 
 </head>
 
-<body>
+<body bgcolor="#e6fafe">
 	<div>
-		<p class="text-center"><a href="index.php"><img src="/img/rogo_v2.svg" width="90" height=""/></a></p>
+		<p class="text-center"><a href="index.php"><img src="img/rogo_v2.svg" width="90" height=""/></a></p>
 		<p class="subtitle">Local Area Wireless Power Input System</p>
 	</div>
 	<hr class="customHR_1">
 	<div>
 		<h1 color="blue"></h1>
 	</div>
-	<p class="send_button">send</p>
+	<div>
+		<form name="lawpis_core" action="index.php" method="post">
+			<p class="subtitle_2">Select target MAC address</p>
+			<center>
+			<select size="5" name="MAC_Addr" class="select_1">
+			<option value="num1">sample_1 FF:FF:FF:FF:FF:FF</option>
+			<option value="num2">sample_2 FF:FF:FF:FF:FF:FF</option>
+			<option value="num3">sample_3 FF:FF:FF:FF:FF:FF</option>
+			<!--tion value="num4"><input type="text" name="num5"></option-->
+			</select>
+			<br>
+			<br>
+			<p><input type="submit" value="send" class="send_button"/></p>
+			</center>
+		</form>
+	</div>
+	<!-- <p class="send_button">send</p> -->
 </body>
 
 </html>
