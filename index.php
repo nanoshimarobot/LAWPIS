3f2e4f832a5c7c4ed4222acf8bbd5cfbab23d9ad
nanoshimarobot <1176641@st.anan-nct.ac.jp>
2020-07-24 23:56:09 +0900

    Use exec() with sudo on PHP

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+$array = array(1,2,3,4,5,6,7,8,9,10,'fuck ');
+$host = "192.168.3.";
+$port = 80;
+$timeout = 1;
+$tag_array;
+$output = 0;
+	for($i = 1; $i <= 20; $i++){
+		$output = shell_exec("sudo arping -c 1 -i wlan0 192.168.3.".$i);
+		var_dump($output);
+		#$port_checker = fSockOpen($host+$i, $port, $errno, $errorstr, $timeout);
+		#$res = ($port_checker)? 1 : 0;
+		#echo $res;
+		#if($res === 1){
+			#$tag_array[] = $host+$i;
+		#}
+		#fclose($port_checker);
+	}
+#var_dump($output);
+?>
 <!doctype html>
 <html>
 
@@ -24,14 +44,14 @@
 			<select size="5" name="MAC_Addr" class="select_1">
 			<?php
 			class test_class{
-				public function dumper($input1,$input2){
-					echo '<option value="num'.$input1.'">sample'.$input1.' '.$input2.'</option>';
+				public function dumper($input1,$input2, $array){
+					echo '<option value="num'.$input1.'">'.$array[$input1].'sample'.$input1.' '.$input2.'</option>';
 				}
 			}
 			$test = new test_class();
 			
-				for($i = 1; $i <= 10; $i++){
-					$test->dumper($i,'FF:FF:FF:FF:FF:FF');
+				for($i = 0; $i <= 10; $i++){
+					$test->dumper($i,'FF:FF:FF:FF:FF:FF',$tag_array);
 				}
 			?>
 			<!--<option value="num1">sample_1 FF:FF:FF:FF:FF:FF</option>
