a597c228c89a0315dc7194fd2929cfde3f5c9b64
nanoshimarobot <1176641@st.anan-nct.ac.jp>
2020-08-12 03:55:05 +0900

    Added a manual entry field for addresses

--- php/index.php
+++ php/index.php
@@ -1,40 +1,28 @@
 <?php
-#$host = "192.168.3.";
-#$port = 80;
 $index = 0;
 $tag_array;
 $output;
 exec("sudo arp-scan -I eth1 -l",$output);
 foreach($output as $key => $value){
-		#echo strpos($value, 'Ending')."<br>";
 	if($key != 0 && $key != 1 && strpos($value,'Ending') === false && strpos($value,'packet',3) === false && $value != NULL){
-		#echo $value."<br>";
 		$tag_array[$index] = $value;
 		$index++;
 	}
-}	
-	#$port_checker = fSockOpen($host+$i, $port, $errno, $errorstr, $timeout);
-		#$res = ($port_checker)? 1 : 0;
-		#echo $res;
-		#if($res === 1){
-			#$tag_array[] = $host+$i;
-		#}
-		#fclose($port_checker);
-#var_dump($output);
+}
 ?>
 <!doctype html>
 <html>
 
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="./style_sheet/style_sheet.css" />
+	<link rel="stylesheet" type="text/css" href="../style_sheet/style_sheet.css" />
 	<title>LAWPIS_home</title>
 
 </head>
 
 <body bgcolor="#e6fafe">
 	<div>
-		<p class="text-center"><a href="index.php"><img src="img/rogo_v2.png" width="390" height=""/></a></p>
+		<p class="text-center"><a href="index.php"><img src="../img/rogo_v2.png" width="390" height=""/></a></p>
 	<!--	<p class="subtitle">Local Area Wireless Power Input System</p>-->
 	</div>
 	<hr class="customHR_1">
@@ -51,11 +39,9 @@ foreach($output as $key => $value){
 				echo '<option value="'.$value.'">'.$value.'</option>';
 			}
 			?>
-			<!--<option value="num1">sample_1 FF:FF:FF:FF:FF:FF</option>
-			<option value="num2">sample_2 FF:FF:FF:FF:FF:FF</option>
-			<option value="num3">sample_3 FF:FF:FF:FF:FF:FF</option>-->
-			<!--tion value="num4"><input type="text" name="num5"></option-->
 			</select>
+			<p class="subtitle_3">Type your MAC address if not displayed</p>
+			<input type="text" name="MAC_Addr_manual" minlength="17" maxlength="17" size="10">
 			<br>
 			<br>
 			<p><input type="submit" value="send" class="send_button"/></p>
@@ -66,24 +52,3 @@ foreach($output as $key => $value){
 </body>
 
 </html>
-<?php
-/*error_reporting(E_ALL);
-set_time_limit(0);
-ob_implicit_flush();
- $port = 49513;
- $addr = '192.168.3.25';
- $from = '';
-	if(($sock = socket_create(AF_INET,SOCK_DGRAM,SOL_UDP)) === false){
-		echo "socket_create() failed: reason" . socket_strerror(socket_last_error()) . "\n";
-	}
-	if((socket_bind($sock,$addr,$port)) === false){
-		echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
-	}
-	while(true){
-		socket_recvfrom($sock,$buf,4096,0,$from,$port);
-		$recv_data = unpack("Smessage",$buf);
-		print($recv_data["message"]);
-	}
-	socket_close($sock);
- */	
-?>
--- php/wakeup.php
+++ php/wakeup.php
@@ -7,27 +7,33 @@ $port = 10;
 $addr = '255.255.255.255';
 
 $selected_target = (string)$_POST["MAC_Addr"];
+$target_MAC_Addr_manual = (string)$_POST["MAC_Addr_manual"];
 $temp = explode("\t",$selected_target);
 $target_MAC_Addr = "80:fa:5b:50:be:bd";
 echo $target_MAC_Addr."<br>";
 $target_MAC_Addr = explode(":",$target_MAC_Addr);
+$target_MAC_Addr_manual = explode(":",$target_MAC_Addr_manual);
 foreach($target_MAC_Addr as $tag){
 	echo $tag."<br>";
 }
 $packet = "";
+$packet2 = "";
 for($i = 0; $i < 6; $i++){
 	$packet .= chr(0xFF);
+	$packet2 .= chr(0xFF);
 }
 for($i = 0; $i < 16; $i++){
 	for($j = 0; $j < 6; $j++){
 		$packet .= chr(hexdec($target_MAC_Addr[$j]));
+		$packet2 .= chr(hexdec($target_MAC_Addr_manual[$j]));
 	}
 }
-echo strlen($packet);
+#echo strlen($packet);
 if(($sock = socket_create(AF_INET,SOCK_DGRAM,SOL_UDP)) === false){
 	echo "socket_create() failed: reason" . socket_strerror(socket_last_error()) . "\n";
 }
 socket_set_option($sock,SOL_SOCKET,SO_BROADCAST,1);
 socket_sendto($sock,$packet,strlen($packet),0,$addr,$port);
+socket_sendto($sock,$packet2,strlen($packet2),0,$addr,$port);
 socket_close($sock);
 ?>
